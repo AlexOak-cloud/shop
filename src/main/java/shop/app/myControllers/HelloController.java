5abe0e000f6f6d8b121fb857bbd3363a9da22640
AlexOak <dubovec995@gmail.com>
2021-10-15 08:52:22 +0200

    create

--- src/main/java/shop/app/myControllers/HelloController.java
+++ src/main/java/shop/app/myControllers/HelloController.java
@@ -1,4 +1,12 @@
 package shop.app.myControllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
 public class HelloController {
+
+    @GetMapping
+    public ModelAndView start(){
+        return new ModelAndView("start.html");
+    }
 }
