e5505f17c085e2516c5389034bae6f24e7a9222e
AlexOak <dubovec995@gmail.com>
2021-10-15 10:11:38 +0200

    chechMVC

--- src/main/java/shop/app/Entity/Product.java
+++ src/main/java/shop/app/Entity/Product.java
@@ -1,9 +1,56 @@
 package shop.app.Entity;
 
+import javax.persistence.*;
+import java.util.Objects;
 
+@Table(name = "product")
+@Entity
+public class Product {
 
-public class Product extends AbstractProduct{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "price")
+    private int price;
 
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
 
+    public Product() {
+    }
 
+    @Override
+    public String toString() {
+        return id + ") " + name + " - " + price + "p.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
--- src/main/java/shop/app/myControllers/HelloController.java
+++ src/main/java/shop/app/myControllers/HelloController.java
@@ -1,12 +1,19 @@
 package shop.app.myControllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import shop.app.Entity.Product;
 
+@Controller
 public class HelloController {
 
     @GetMapping("/start")
     public ModelAndView start(){
-        return new ModelAndView("start.html");
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("start.html");
+        Product product = new Product("Milk",12);
+        mav.addObject("product",product);
+        return mav;
     }
 }
