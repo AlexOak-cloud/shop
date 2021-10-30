a641667bc82ea6a7f9952f807cdb0ed45c245a92
AlexOak <dubovec995@gmail.com>
2021-10-30 12:51:33 +0200

    create-add

--- src/main/java/shop/app/entity/Product.java
+++ src/main/java/shop/app/entity/Product.java
@@ -1,18 +1,10 @@
 package shop.app.entity;
 
-
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
-
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "product")
-@NoArgsConstructor
-@Data
-@AllArgsConstructor
 public class Product {
 
     @Id
@@ -23,4 +15,57 @@ public class Product {
     @Column(name = "price")
     private int price;
 
+    public Product() {
+    }
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id && price == product.price && Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
